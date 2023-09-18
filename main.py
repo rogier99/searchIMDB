ad5dcbfef7dbb7860ee18ca586efad98597972b6
Rogier van den Burgh <rogier99r@outlook.com>
2023-09-18 12:25:15 +0200

    year search prototype is working and try except statements are added.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import imdb
-from imdb import Cinemagoer
+from imdb import Cinemagoer, IMDbError
 
 
-def movie_info (movie_id):
+def movie_info(movie_id):
     # Get the list of movies and get our movie
-    ia = imdb.Cinemagoer()
-    movie = ia.get_movie(movie_id)
+    ia = Cinemagoer()
+    try:
+        int(movie_id)
+    except ValueError:
+        print("Not a valid movie id is provided.")
+        return
+
+    try:
+        movie = ia.get_movie(movie_id)
+    except IMDbError:
+        print("Movie id " + movie_id + " could not be found.")
+        return
+
     # Every movie consists of main, plot and synopsis
     # Go over every element and print the content of these lists
     for dic in movie.infoset2keys:
@@ -13,44 +23,62 @@ def movie_info (movie_id):
             print("\n" + str(item))
             print(movie.get(item))
 
-def title_search (keyword):
+
+def title_search(keyword):
     # Get the list of movies and find moves with given keyword
-    ia = imdb.Cinemagoer()
-    movies = ia.search_movie(keyword)
+    ia = Cinemagoer()
+
+    try:
+        movies = ia.search_movie(keyword)
+    except IMDbError:
+        print("The movie with keyword " + keyword + " in the title could not be found.")
+        return
 
     # Print the results
     for movie in movies:
         print(movie)
 
-def director_search (director):
+
+def director_search(director):
     # Get the list of directors that are similar to the given input
-    ia = imdb.Cinemagoer()
-    persons = ia.search_person(director)
+    ia = Cinemagoer()
+
+    try:
+        persons = ia.search_person(director)
+    except IMDbError:
+        print("The person " + director + " could not be found.")
+        return
 
     # For every person, print the movies they directed
     for person in persons:
         print("Looking for movies from: " + str(person))
         print(person.get('director'))
 
-def get_year(movie):
-    movie['year']
 
-def year_search (year):
-    ia = imdb.Cinemagoer()
-    # Get the list of all the movies <= This part I cannot find anywhere, just get by id or keyword and executing ia.get_movie for every id will be really slow
+def get_ranking(movie):
+    return movie['rating']
 
-    # For every movie, look at the year (preferably looking from right to left (most movies start with either 1 or 2, the last two digits are more "unique") )
-    for i in range(0, reversed(len(movies) - 1)):
-        if movies[i]['year'] != year:
+
+def year_search(year):
+    ia = Cinemagoer()
+    # Get the list of all the movies, as even the top_100 is empty we use 3 movies, 2 from 1909, one from 1921
+    movies = [ia.get_movie('1000'), ia.get_movie('1001'), ia.get_movie('12345')]
+    # For every movie, look at the year
+    for i in reversed(range(0, len(movies))):
+        if movies[i]['year'] != int(year):
             movies.pop(i)
-            
+
     # Use sort the movies by ranking
-    movies.sort(key=get_year)
+    if len(movies) == 0:
+        print("There are no movies of year " + str(year) + " found")
+        return
+
+    movies.sort(key=get_ranking)
 
     # Print the list
+    print("Sorted list:")
     for movie in movies:
         print(movie)
-
 
 
 def main():
@@ -58,27 +86,28 @@ def main():
     print("1. Get movie information from id.")
     print("2. Get movie titles from a keyword.")
     print("3. Get movies made by given director.")
-    print("4. Get movies orderd by ranking in given year.")
+    print("4. Get movies ordered by ranking in given year.")
     option = input()
     if option == '1':
         print("Give movie id.")
         movie_id = input()
         movie_info(movie_id)
-    
+
     if option == '2':
         print("Give keyword.")
         keyword = input()
         title_search(keyword)
-        
+
     if option == '3':
         print("Give director.")
         director = input()
         director_search(director)
-    
+
     if option == '4':
         print("Give year.")
         year = input()
         year_search(year)
+
 
 if __name__ == "__main__":
     main()
